9ef39c90d5194dab67d3bac5d0b2d9be6e08e8fc
Jiakun Ma <jkma@actions-semi.com>
2018-09-03 10:54:38 +0800

    修改：     second/longestvalidparentless.cpp

--- second/longestvalidparentless.cpp
+++ second/longestvalidparentless.cpp
@@ -13,7 +13,35 @@ using namespace std;
 class Solution {
     public:
         int longestValidParentheses(string s) {
-            int max = 0, size = s.size(), i;
+            int size = s.size(), i, j, max = 0;
+
+            if(size < 2)
+                return max;
+
+            int dp[size];
+            memset(dp, 0, sizeof(dp));
+
+            for(i = 1;i < size;i++) {
+                if(s[i] == ')' && s[i - 1] == '(') {
+                    if(i > 1)
+                        dp[i] += dp[i - 2];
+                    dp[i] += 2;
+                } else if(s[i] == ')') {
+                    j = dp[i - 1];
+                    if(j != 0) {
+                        if(i - j - 1 >= 0) {
+                            if(s[i - j - 1] == '(') {
+                                dp[i] = dp[i - 1] + 2;
+                                if(i - j - 2 >= 0)
+                                    dp[i] += dp[i - j - 2];
+                            }
+                        }
+                    }
+                }
+
+                if(dp[i] > max)
+                    max = dp[i];
+            }
 
             return max;
         }
